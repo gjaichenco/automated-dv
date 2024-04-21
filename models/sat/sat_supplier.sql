e86947645dd09c7ee5283d6b1a9a04541f3dbd9f
Gustavo Andres Jaichenco <gustavojaichenco@gmail.com>
2024-04-21 21:50:55 +0000

    commit_dvault_V1

---
+++ models/sat/sat_supplier.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    schema='SAT'
+  )
+}}
+
+
+
+{%- set source_model = "stg_supplier" -%}
+{%- set src_pk = "SUPP_PK" -%}
+{%- set src_hashdiff = "SUPP_HASHDIFF" -%}
+{%- set src_payload = [
+        'S_ACCTBAL'
+      , 'S_ADDRESS'
+      , 'S_PHONE'
+      , 'S_COMMENT'
+      , 'S_NAME'
+ ] -%}
+
+{%- set src_ldts = "LOAD_DATE" -%}
+{%- set src_source = "RECORD_SOURCE" -%}
+
+{{ automate_dv.sat(src_pk=src_pk, src_hashdiff=src_hashdiff,
+                   src_payload=src_payload, src_eff=none,
+                   src_ldts=src_ldts, src_source=src_source,
+                   source_model=source_model) }}
